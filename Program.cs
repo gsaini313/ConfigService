c58cbff0d4fad61d31c3a20b487854005f47cfce
Gaurav Saini <gsaini313@gmail.com>
2022-04-25 11:44:40 -0400

    Added OpenShift Support

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration; // OpenShift Line 1
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConfigService
 {
@@ -13,7 +15,19 @@ namespace ConfigService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build(); //OpenShift 2nd line added
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080"; //OpenShift 3rd line added
+
+            var host = new WebHostBuilder()
+             .UseKestrel()
+             .UseContentRoot(Directory.GetCurrentDirectory())
+             .UseIISIntegration()
+            // .UseStartup()
+             .UseUrls(url) //OpenShift 4th line added
+             .Build();
+
+             host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
