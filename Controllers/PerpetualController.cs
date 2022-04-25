1019e757a0e290936f0f50fa76f6aec4518d8856
Gaurav Saini <gsaini313@gmail.com>
2022-04-25 11:12:46 -0400

    Add project files.

---
+++ Controllers/PerpetualController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConfigService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PerpetualController : ControllerBase
+    {
+        // GET api/<PerpetualController>/5
+        [HttpGet]
+        public object Get()
+        {
+            var yamlfile = new StreamReader("ConfigSettings\\Perpetual\\provision_perpetual_config.yml");
+            var yaml = new Deserializer();
+            var Resultobject = yaml.Deserialize(yamlfile);
+
+            return Resultobject;
+        }
+    }
+}
